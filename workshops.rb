e9b5741c0495671ded0950b02c8be50a5f62eb74
Guillermo Diaz <diazgdev@gmail.com>
2021-11-26 19:53:29 -0600

    now the create form can save new workhops

--- workshops.rb
+++ workshops.rb
@@ -6,6 +6,12 @@ rescue Errno::ENOENT
   nil
 end
 
+def save_workshop(name, description)
+  File.open("workshops/#{name}.txt", 'w') do |file|
+    file.print(description)
+  end
+end
+
 get '/' do
   @files = Dir.entries('workshops')
   erb :home
@@ -19,4 +25,8 @@ get '/:name' do
   @name = params[:name]
   @description = workshop_content(@name)
   erb :workshop
+end
+
+post '/create' do
+  save_workshop(params['name'], params['description'])
 end
